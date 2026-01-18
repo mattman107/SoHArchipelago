1177c7dcef543e69283c36a9412e54f501e41403
mattman107 <mathoman@ymail.com>
2026-01-17 22:16:26 -0500

    Add option to slot data

--- worlds/oot_soh/__init__.py
+++ worlds/oot_soh/__init__.py
@@ -447,5 +447,6 @@ class SohWorld(World):
             "no_logic": self.options.true_no_logic.value,
             "apworld_version": self.apworld_version,
             "enable_all_tricks": self.options.enable_all_tricks.value,
-            "tricks_in_logic": self.options.tricks_in_logic.value
+            "tricks_in_logic": self.options.tricks_in_logic.value,
+            "shuffle_bean_souls": self.options.shuffle_bean_souls.value
         }
