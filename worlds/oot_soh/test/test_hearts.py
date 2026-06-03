935efb5ca1c696c34cfe7f597b47367b63cd7cab
mattman107 <65982675+mattman107@users.noreply.github.com>
2026-06-03 12:40:51 -0400

    Add other heart unit tests

--- worlds/oot_soh/test/test_hearts.py
+++ worlds/oot_soh/test/test_hearts.py
@@ -1,6 +1,7 @@
 from .. import SohWorld
 from ..Items import Items
 from .bases import SohTestBase
+from BaseClasses import ItemClassification as IC
 
 
 class TestHearts(SohTestBase):
@@ -31,3 +32,69 @@ class TestHearts(SohTestBase):
 
         self.remove(heart)
         self.assertTrue(self.multiworld.state.soh_heart_count[self.player] == 4)  # type: ignore
+
+class TestHeartsMinimal(SohTestBase):
+    options = {"starting_hearts": 1, "item_pool": "minimal"}
+    def test_collecting_all_hearts_count(self):
+        self.assertEqual(self.multiworld.state.soh_heart_count[self.player], 1, "start with 1 heart") # type: ignore 
+        self.collect_by_name([Items.PIECE_OF_HEART, Items.PIECE_OF_HEART_WINNER, Items.HEART_CONTAINER])
+        self.assertEqual(self.multiworld.state.soh_heart_count[self.player], 3, "after collecting all heart pieces and containers we should have 3 hearts") # type: ignore 
+
+    def test_count_heart_pieces_progression(self):
+        poh = list()
+        winner = list()
+        for item in self.multiworld.itempool:
+            if item.name == Items.PIECE_OF_HEART and (item.classification & IC.progression) == IC.progression:
+                poh.append(item)
+            if item.name == Items.PIECE_OF_HEART_WINNER and (item.classification & IC.progression) == IC.progression:
+                winner.append(item)    
+        self.assertEqual(len(poh), 3, "Should have been 3 PoH")
+        self.assertEqual(len(winner), 1, "Should have been 1 Winner PoH")
+
+    def test_count_heart_total(self):
+        poh = list()
+        winner = list()
+        container = list()
+        for item in self.multiworld.itempool:
+            if item.name == Items.PIECE_OF_HEART:
+                poh.append(item)
+            if item.name == Items.PIECE_OF_HEART_WINNER:
+                winner.append(item)    
+            if item.name == Items.HEART_CONTAINER:
+                container.append(item)   
+        self.assertEqual(len(container), 1, "Should have been 0 Containers")
+        self.assertEqual(len(poh), 3, "Should have been 3 PoH")
+        self.assertEqual(len(winner), 1, "Should have been 1 Winner PoH")
+
+class TestHeartsScarce(SohTestBase):
+    options = {"starting_hearts": 1, "item_pool": "scarce"}
+    def test_collecting_all_hearts_count(self):
+        self.assertEqual(self.multiworld.state.soh_heart_count[self.player], 1, "start with 1 heart") # type: ignore 
+        self.collect_by_name([Items.PIECE_OF_HEART, Items.PIECE_OF_HEART_WINNER, Items.HEART_CONTAINER])
+        self.assertEqual(self.multiworld.state.soh_heart_count[self.player], 12, "after collecting all heart pieces and containers we should have 12 hearts") # type: ignore 
+
+    def test_count_heart_pieces_progression(self):
+        poh = list()
+        winner = list()
+        for item in self.multiworld.itempool:
+            if item.name == Items.PIECE_OF_HEART and (item.classification & IC.progression) == IC.progression:
+                poh.append(item)
+            if item.name == Items.PIECE_OF_HEART_WINNER and (item.classification & IC.progression) == IC.progression:
+                winner.append(item)    
+        self.assertEqual(len(poh), 8, "Should have been 8 PoH")
+        self.assertEqual(len(winner), 0, "Should have been 0 Winner PoH")
+
+    def test_count_heart_total(self):
+        poh = list()
+        winner = list()
+        container = list()
+        for item in self.multiworld.itempool:
+            if item.name == Items.PIECE_OF_HEART:
+                poh.append(item)
+            if item.name == Items.PIECE_OF_HEART_WINNER:
+                winner.append(item)    
+            if item.name == Items.HEART_CONTAINER:
+                container.append(item)   
+        self.assertEqual(len(container), 0, "Should have been 0 Containers")
+        self.assertEqual(len(poh), 43, "Should have been 43 PoH")
+        self.assertEqual(len(winner), 1, "Should have been 1 Winner PoH")
